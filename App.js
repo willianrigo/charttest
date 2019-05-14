63263329fb6272afe023347311fe923851ee1991
Nilton Lopes <niltonrck@gmail.com>
2019-05-14 19:48:47 -0300

    char test

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AreaChartExample from './Chart'
+import LayeredChartsExample from './LayeredCharts'
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+      <View><Text>Hello world</Text></View>
+      <View>
+        <AreaChartExample/>
+      </View>
+      <View>
+        <LayeredChartsExample/>
+      </View>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
