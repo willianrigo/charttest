86c00b84ec434ba808fd7e74125610ef17824549
Willian Rigo <willianrigo@hero99.com.br>
2019-05-15 13:10:08 -0300

    adding x-axis labels

--- GroupedBarCharts.js
+++ GroupedBarCharts.js
@@ -1,15 +1,47 @@
 import React from 'react'
+import {View} from 'react-native'
 import { BarChart, Grid, XAxis } from 'react-native-svg-charts'
 
-export default class GroupedBarCharts extends React.PureComponent {
+class GroupedBarCharts extends React.PureComponent {
 
     render() {
 
         const labels = [ 'jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
-        const data1 = [ 14, 1, 100, 95, 94, 24, 8, 85, 91, 35, 53, 53, 78, 66, 96, 33, 26, 32, 73, 8 ]
-            .map((value) => ({ value }))
-        const data2 = [ 24, 28, 93, 77, 42, 62, 52, 87, 21, 53, 78, 62, 72, 6, 89, 70, 94, 10, 86, 84 ]
-            .map((value) => ({ value }))
+            // .map((value) => ({ value }))
+        const dataArray = [ 14, 1, 1000, 95, 94, 24, 8, 85, 91, 35, 53, 53 ]
+        // const data1 = dataArray
+        //     .map((value) => ({ value }))
+        // const data2 = [ 24, 28, 93, 77, 42, 62, 52, 87, 21, 53, 78, 62 ]
+        //     .map((value) => ({ value }))
+        const data1 = [
+            { label: 'Jan', value: 500 },
+            { label: 'Feb', value: 312 },
+            { label: 'Mar', value: 424 },
+            { label: 'Apr', value: 745 },
+            { label: 'May', value: 89 },
+            { label: 'Jun', value: 434 },
+            { label: 'Jul', value: 650 },
+            { label: 'Aug', value: 980 },
+            { label: 'Sep', value: 123 },
+            { label: 'Oct', value: 186 },
+            { label: 'Nov', value: 689 },
+            { label: 'Dec', value: 643 }
+          ]
+
+          const data2 = [
+            { label: 'Jan', value: 500 },
+            { label: 'Feb', value: 312 },
+            { label: 'Mar', value: 424 },
+            { label: 'Apr', value: 745 },
+            { label: 'May', value: 89 },
+            { label: 'Jun', value: 434 },
+            { label: 'Jul', value: 650 },
+            { label: 'Aug', value: 980 },
+            { label: 'Sep', value: 123 },
+            { label: 'Oct', value: 186 },
+            { label: 'Nov', value: 689 },
+            { label: 'Dec', value: 643 }
+          ]
 
         const barData = [
             {
@@ -23,27 +55,40 @@ export default class GroupedBarCharts extends React.PureComponent {
             },
         ]
 
+        console.log(data1.forEach((value) => {
+            // console.log("Object value", Object.keys(value)[0])
+            // console.log("Index: ", dataArray.indexOf(value.value))
+            // console.log("Value: ", labels[dataArray.indexOf(value.value)])
+    
+        }));
+
+
         return (
-            <BarChart
-                style={ { height: 200 } }
-                data={ barData }
-                spacingInner={0.45}
-                yAccessor={({ item }) => item.value}
-                svg={{
-                    fill: 'green',
-                }}
-                contentInset={ { top: 30, bottom: 30 } }
-                { ...this.props }
-            >
-            <XAxis
-                style={{ marginHorizontal: -10 }}
-                data={ labels }
-                formatLabel={ (value, index) => index }
-                contentInset={{ left: 10, right: 10 }}
-                svg={{ fontSize: 10, fill: 'black' }}
-            />
-                <Grid/>
-            </BarChart>
+            
+            <View>
+                <BarChart
+                    style={ { height: 200 } }
+                    data={ barData }
+                    spacingInner={0.45}
+                    yAccessor={({ item }) => item.value}
+                    svg={{
+                        fill: 'green',
+                    }}
+                    contentInset={ { top: 30, bottom: 30, left: 10, right: 10 } }
+                    { ...this.props }
+                >
+                    <Grid/>
+                </BarChart>
+
+                <XAxis
+                    style={{ marginHorizontal: -10 }}
+                    data={ data1 } 
+                    svg={{ fontSize: 10, fill: 'black' }}
+                    formatLabel={(index, value) => labels[index-1]}
+                />
+            </View>
         )
     }
 }
+
+export default GroupedBarCharts;
